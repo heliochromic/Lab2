3d3417b6d4bb8808f224ac13a861891b2019124f
bohdan <howchromium@gmail.com>
2023-04-04 20:43:43 +0300

    huynia

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,42 @@
+import javax.swing.*;
+
 public class Item {
+    private static int id = 0;
+    private int startAmount;
+    private String name;
+    private JButton delete, edit;
+    Item (String name, JButton delete, JButton edit){
+        this.name = name;
+        this.delete = delete;
+        this.edit = edit;
+        id++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public JButton getDelete() {
+        return delete;
+    }
+
+    public void setDelete(JButton delete) {
+        this.delete = delete;
+    }
+
+    public JButton getEdit() {
+        return edit;
+    }
+
+    public void setEdit(JButton edit) {
+        this.edit = edit;
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,40 +1,52 @@
 import javax.swing.*;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import static java.lang.reflect.Array.getLength;
 
 public class Program extends JFrame implements ActionListener {
 
-    JScrollPane items;
-    JPanel scrollPanel, buttonPanel, backgroundPanel;
-    ArrayList<JPanel> items_list;
+    JPanel scrollPanel, buttonPanel, backgroundPanel, s, a, st;
+    ArrayList<JPanel> items_list = new ArrayList<>();
     JTextField search_bar;
     JButton search, add_group, add_item, stats;
     JTabbedPane tabbedPane;
-    GridBagLayout layout;
-    GridBagConstraints constraints;
+    GridBagLayout layout, button_layout;
+    GridBagConstraints constraints, button_constraints;
 
     public Program() {
         setName("vidrah?");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-
         init();
-
         setVisible(true);
     }
 
     private void init() {
 
+
+        //тут все норм
+
+        tabbedPane = new JTabbedPane();
+        createTab("candies");
+        createTab("diary");
+        for (JPanel i : items_list){
+            tabbedPane.addTab("wow",i);
+        }
+
+        //
         scrollPanel = new JPanel();
         scrollPanel.setPreferredSize(new Dimension((int) (this.getWidth() * 0.7), this.getHeight()));
 
+        scrollPanel.add(tabbedPane);
 
         search_bar = new JTextField();
         search = buttonNormalization(new JButton("Search"));
@@ -42,16 +54,29 @@ public class Program extends JFrame implements ActionListener {
         add_item = buttonNormalization(new JButton("Add item"));
         stats = buttonNormalization(new JButton("Statistics"));
 
-        buttonPanel = new JPanel(new GridLayout(5,1));
-        buttonPanel.setBackground(Color.BLACK);
+        s = new JPanel();
+        a = new JPanel();
+        st = new JPanel();
+
+        s.add(search_bar);
+        s.add(search);
+        a.add(add_group);
+        a.add(add_item);
+        st.add(stats);
+
+        buttonPanel = new JPanel(new GridLayout(7, 1));
+        buttonPanel.setBackground(new Color(66, 48, 132));
         buttonPanel.setPreferredSize(new Dimension(((int) (this.getWidth() * 0.3)), this.getHeight()));
-        buttonPanel.add((search_bar));
+        buttonPanel.add(new JLabel(""));
+        buttonPanel.add(search_bar);
+        search_bar.setBorder(BorderFactory.createLineBorder(new Color(66, 48, 132), 5));
+        search_bar.setFont(new Font("Arial", Font.BOLD, 16));
         buttonPanel.add(search);
         buttonPanel.add(add_group);
         buttonPanel.add(add_item);
         buttonPanel.add(stats);
 
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(60,5,60,5));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(40, 10, 20, 10));
 
         layout = new GridBagLayout();
         constraints = new GridBagConstraints();
@@ -69,13 +94,64 @@ public class Program extends JFrame implements ActionListener {
         this.pack();
     }
 
-    public JButton buttonNormalization(JButton button){
-        button.setBackground(new Color(192, 192, 192));
+    public JButton buttonNormalization(JButton button) {
+        button.setBackground(new Color(139, 135, 220));
         button.setFont(new Font("Arial", Font.BOLD, 16));
-        button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        button.setFocusPainted(false);
+        button.setBorder(BorderFactory.createLineBorder(new Color(66, 48, 132), 5));
+        //button.setFocusPainted(false);
         return button;
     }
+
+    public void createTab(String name) {
+        String filename = "";
+        try {
+            filename = name + ".txt";
+            File f = new File(filename);
+            if (f.createNewFile()) System.out.println("New " + filename + " was created");
+            else System.out.println("File already exists.");
+        } catch (IOException e) {
+            System.out.println("damn");
+            e.printStackTrace();
+        }
+        items_list.add(createScrollableTable(filename));
+    }
+
+    public JPanel createScrollableTable(String filename){
+        String[] columnNames = {"id","item","amount","buy","edit","delete"};
+        //тут треба, щоб з файлу діставалися продукти і закидувалися в таблицю
+        Object[][] rowData = {
+                {1,"Ketchup",10,new JButton("✔"),new JButton("✖"), new JButton("✍️")},
+                {2,"Ketchup",10,new JButton("✔"),new JButton("✖"), new JButton("✍️")},
+                {3,"Ketchup",10,new JButton("✔"),new JButton("✖"), new JButton("✍️")},
+                {4,"Ketchup",10,new JButton("✔"),new JButton("✖"), new JButton("✍️")}
+        };
+        //Подивитися як закидуватися в таблицю кнопки чи переробити це все з GridBagLayout
+        //
+        DefaultTableModel model = new DefaultTableModel(rowData, columnNames);
+        JTable table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+        JPanel p = new JPanel();
+        p.add(scrollPane);
+        return p;
+    }
+
+    public void addItemToFile(String filename, String item_name) {
+        try {
+            FileOutputStream fos = new FileOutputStream(filename, true);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            //oos.writeObject();
+            //додавати об'єкт, зробити як по кнопочці, так і заготовку в різних табах
+        } catch (IOException e) {
+            System.out.println("damn");
+            e.printStackTrace();
+        }
+    }
+
+    //public void getItemsFromFile(String filename)
+    //тут треба буде отримувати item по стрінгу idk, я вже попаяний розуміти цей код,
+    // як на цій залупі можна було model comp це йобана таємниця всесвітнього масштабу
+    //от хочеться просто людського написати це і забути, бо просто придумуєш собі ідею, а потім хочеш відкинутися в намаганнях її реалізуватию
+
 
     /**
      * Invoked when an action occurs.
